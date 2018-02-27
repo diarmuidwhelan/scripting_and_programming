d15dd3e178a70dca54280966ccdf52427b0cb075
diarmuidwhelan <36341007+diarmuidwhelan@users.noreply.github.com>
2018-02-27 22:45:35 +0000

    Create euler_problem5.py

---
+++ euler_problem5.py
@@ -0,0 +1,11 @@
+#Diarmuid Whelan, 2018-02-25
+#Euler Problem 5
+
+smallest_num = 1
+for i in range (1,21):
+    if smallest_num % i > 0: # If the number is not divisible by i
+        for k in range (1,21):
+            if (smallest_num * k) % i == 0: # Find the smallest number divisible by i    
+                smallest_num = smallest_num * k
+                break
+print (smallest_num)
