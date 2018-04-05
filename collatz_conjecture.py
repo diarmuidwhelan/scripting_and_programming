c5c1d667bad6bd064ae5698899ba17834ea65538
Your Name <diarmuidwhelan@gmail.com>
2018-04-05 22:38:08 +0100

    Added request for integer

--- collatz_conjecture.py
+++ collatz_conjecture.py
@@ -3,11 +3,18 @@
 #Diarmuid Whelan 2018-02-10
 #Collatz Conjecture Exercise
 #https://en.wikipedia.org/wiki/Collatz_conjecture
+#Complete the exercise discussed in the Collatz conjecture video by writing a single Python script that 
+#starts with an integer and repeatedly applies the Collatz function (divide by 2 if even, multiply by three 
+#and 1 if odd) using a while loop and if statement. At each iteration, the current value of the integer
+#should be printed to the screen.
+#If you wish to enhance your program, have the program ask the user for the integer 
+#instead of specifying a value at the start of your code.
 #Will use 1992(year I was born) to test
 #################################################################################
 
 
-n = 1992 
+ 
+n=int(input("Please enter number:")) #Enter an integer
 while n >1:    #continue until n=1
     if n % 2 == 0:  #if n is divisible evenly by 2 do so 
         n=n/2
